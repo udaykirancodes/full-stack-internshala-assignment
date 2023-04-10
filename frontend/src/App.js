e9af127a67608ea22a1be1ba8b9f1bc35ed89500
Udaykiran <officialudaykiran@gmail.com>
2023-04-10 13:59:20 +0530

    Started

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login"
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Dashboard />} exact path="/" />
+          <Route element={<Login />} exact path="/login" />
+          <Route element={<Register />} exact path="/register" />
+        </Routes>
+
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
