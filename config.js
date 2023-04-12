f42b4b37d07c949419483a0083ab7419f91d567c
Udaykiran <officialudaykiran@gmail.com>
2023-04-12 13:08:36 +0530

    MongoURL

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ dotenv.config();
 module.exports = {
     port: process.env.PORT || 8000,
     mongoose: {
-        // url: 'mongodb+srv://udaykirancodes:udaykirancodes@cluster0.ip9n0c4.mongodb.net/codesaver?retryWrites=true&w=majority',
-        url: 'mongodb://localhost:27017/assignment?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+        url: 'mongodb+srv://udaykirancodes:udaykirancodes@cluster0.ip9n0c4.mongodb.net/merntask?retryWrites=true&w=majority',
+        // url: 'mongodb://localhost:27017/assignment?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
         options: {
             useNewUrlParser: true,
         }
